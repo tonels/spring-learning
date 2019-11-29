7d382a09a4b9bbdcc9c56d797c7ef3b9659f38cd
liangshuai <1589900136@qq.com>
2019-11-29 10:59:01 +0800

    init

---
+++ CommandLineRunner/src/main/java/com/spring/runner/dao/CityRepo.java
@@ -0,0 +1,9 @@
+package com.spring.runner.dao;
+
+import com.spring.runner.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepo extends JpaRepository<City, Long> {
+}
